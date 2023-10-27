ff0c5031d007300350efa2fe2810a215f9ae2c46
Ameneh Yazdani <amene.yazdanii@gmail.com>
2023-10-27 04:14:40 +0330

    Update

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,5 @@
 for file in *.html
     do
         perl -i -pe's/<\/body>/<\/body>\n<script src=".\/js\/main.js"><\/script>/g' "$file"
-        perl -i -pe's/<\/head>/\n<link href="https:\/\/fonts.googleapis.com\/css?family=Lato:400,700" rel="stylesheet" type="text\/css"><\/head>/g' "$file"
+        perl -i -pe's/<\/head>/\n<link href="https:\/\/fonts.googleapis.com\/css?family=Nunito:400,700" rel="stylesheet" type="text\/css">\n<link href="https:\/\/fonts.googleapis.com\/css?family=Quicksand:400,700" rel="stylesheet" type="text\/css"><\/head>/g' "$file"
     done
